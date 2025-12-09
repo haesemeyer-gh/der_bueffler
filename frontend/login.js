38bcde21c846b930bfe90af2cd0d0a6dd8880f50
Niko Häsemeyer <n.haesemeyer@bbw-fi.de>
2025-12-09 14:32:26 +0100

    init anmeldung
    und eine CSS datei als kleinen bonus, bitte nicht hundert draus machen

---
+++ frontend/login.js
@@ -0,0 +1,76 @@
+const APIURL = 'http://localhost:8080';
+
+const loginEl = document.getElementById('login');
+const mainEl = document.getElementById('main');
+
+let token;
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+let cookieToken = getCookie(`token`);
+if (cookieToken) {
+    token = cookieToken;
+    logIn();
+}
+
+const registerButton = document.getElementById('register-button');
+const loginButton = document.getElementById('login-button');
+const emailEl = document.getElementById('login-mail');
+const passEl = document.getElementById('login-pass');
+const unameEl = document.getElementById('register-uname');
+const statusEl = document.getElementById('login-status')
+
+registerButton.addEventListener('click', () => {
+    fetch(APIURL+"/auth/register", {
+        method: "POST",
+        body: JSON.stringify({
+            uname: unameEl.value,
+            email: emailEl.value,
+            password: passEl.value
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    }).then((response) => response.json()).then((response) => {
+        statusEl.innerText = response.message;
+    });
+});
+
+loginButton.addEventListener('click', () => {
+    fetch(APIURL+"/auth/login", {
+        method: "POST",
+        body: JSON.stringify({
+            email: emailEl.value,
+            password: passEl.value
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    }).then(async (response) => {
+        let json = await response.json();
+        if (response.status === 200) {
+            token = json.message;
+            document.cookie = `token=${token}`;
+            logIn();
+        } else {
+            statusEl.innerText = json.message;
+        }
+    });
+});
+
+function logIn() {
+    loginEl.classList.toggle("hidden");
+    mainEl.classList.toggle("hidden");
+}
