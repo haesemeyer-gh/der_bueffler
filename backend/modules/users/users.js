ab098d1e9e1c12f340d39b688171b513775fcdc0
Stella Grazius <s.grazius@bbw-fi.de>
2026-01-20 10:14:19 +0100

    user api sql funktion funktionen
    makeTeacher, makeAdmin und delete Teacher

--- backend/modules/users/users.js
+++ backend/modules/users/users.js
@@ -3,3 +3,15 @@ import { query } from '../db/db.js';
 export function getUserMail(userid) {
     return query("SELECT Mail FROM user WHERE ID LIKE ?", [userid]);
 }
+export function makeTeacher(userid) {
+    return query("UPDATE user SET Lehrer = 1 WHERE ID LIKE ?;", [userid]);
+}
+
+export function makeAdmin(userid) {
+    return query("UPDATE user SET Admin = 1 WHERE ID LIKE ?;", [userid]);
+}
+
+export function deleteTeacher(userid) {
+    return query("UPDATE user SET Lehrer = 0 WHERE ID LIKE ?;", [userid]);
+}
+
