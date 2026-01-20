3b307a4dc54dbd6bd1900e4ba66aa59cdb07f105
Niko Häsemeyer <n.haesemeyer@bbw-fi.de>
2026-01-20 07:19:00 +0100

    kalenderansicht ersten tag feststellen

--- frontend/kalender/index.js
+++ frontend/kalender/index.js
@@ -15,8 +15,25 @@ let nowDate = new Date();
 const monthviewCurrentmonth = document.getElementById('monthview-currentmonth');
 const monthviewTbody = document.getElementById('monthview-tbody');
 function updateTable() {
-    var date = new Date(nowDate.getFullYear(), nowDate.getMonth()+monthDiff);
+    let date = new Date(nowDate.getFullYear(), nowDate.getMonth()+monthDiff);
     monthviewCurrentmonth.innerText = `${date.toLocaleString('de-DE', {month: 'long', year: 'numeric'})}`;
+
+    let daysThisMonth = new Date(nowDate.getFullYear(), nowDate.getMonth()+monthDiff+1, 0).getDate();
+    let firstWeekDay = date.getDay();
+
+    monthviewTbody.innerHTML = '';
+
+    let tableFirstRow = document.createElement('tr');
+
+    while (firstWeekDay > 0) {
+        let tableCell = document.createElement('td');
+        tableCell.innerText = "x";
+        tableFirstRow.appendChild(tableCell);
+        firstWeekDay--;
+    }
+
+    monthviewTbody.append(tableFirstRow);
+
 }
 
 updateTable()
