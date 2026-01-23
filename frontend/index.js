56a9bd2d194c2b02ebf8fd6f083773f24897980e
Niko Häsemeyer <n.haesemeyer@bbw-fi.de>
2026-01-23 07:46:14 +0100

    dashboard & detailansicht: async/await

--- frontend/index.js
+++ frontend/index.js
@@ -1,98 +1,99 @@
-const dashboardEl = document.getElementById('dashboard');
-async function updateDashboard() {
-
-	fetch(APIURL+"/appointment/list", {
+async function fetchAPI() {
+	let response = await fetch(APIURL+"/appointment/list-user", {
 		method: "POST",
 		body: JSON.stringify({
 			token: cookieToken,
-			teamid: "4",
 		}),
 		headers: {
 			"Content-type": "application/json; charset=UTF-8"
 		}
-	}).then((response) => response.json()).then((response) => {
-		console.log(response.message)
-		let appointments = response.message.sort((a, b) => {
-			return new Date(a.Datum) - new Date(b.Datum)
-		});
+	});
+	let json = await response.json();
+	return await json.message.sort((a, b) => {
+		return new Date(a.Datum) - new Date(b.Datum)
+	});
+}
 
-		dashboardEl.innerHTML = ``;
-		if (appointments.length <= 0) {
-			let messageEl = document.createElement('h3');
-			messageEl.innerText = 'Es stehen keine Termine an! :D'
-			dashboardEl.appendChild(messageEl)
-		} else {
-			let messageEl = document.createElement('h3');
-			messageEl.innerText = 'Anstehende Termine:';
-			dashboardEl.appendChild(messageEl)
+const dashboardEl = document.getElementById('dashboard');
+async function updateDashboard() {
 
-			let detailsElNow = document.createElement('details');
-			let summaryElNow = document.createElement('summary');
-			let listElNow = document.createElement('ul');
-			summaryElNow.innerText = "Termine der nächsten Woche";
-			detailsElNow.appendChild(summaryElNow);
-			detailsElNow.appendChild(listElNow);
-			detailsElNow.open = true;
-			let detailsElSoon = document.createElement('details');
-			let summaryElSoon = document.createElement('summary');
-			let listElSoon = document.createElement('ul');
-			summaryElSoon.innerText = "Termine der nächsten 30 Tage";
-			detailsElSoon.appendChild(summaryElSoon);
-			detailsElSoon.appendChild(listElSoon);
-			let detailsElLater = document.createElement('details');
-			let summaryElLater = document.createElement('summary');
-			let listElLater = document.createElement('ul');
-			summaryElLater.innerText = "Zukünftige Termine";
-			detailsElLater.appendChild(summaryElLater);
-			detailsElLater.appendChild(listElLater);
+	let appointments = await fetchAPI();
 
-			let nowDate = new Date();
-			let todayDate = Date.parse(new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate()));
-			let dayInMs = 8.64e+7;
+	dashboardEl.innerHTML = ``;
+	if (appointments.length <= 0) {
+		let messageEl = document.createElement('h3');
+		messageEl.innerText = 'Es stehen keine Termine an! :D'
+		dashboardEl.appendChild(messageEl)
+	} else {
+		let messageEl = document.createElement('h3');
+		messageEl.innerText = 'Anstehende Termine:';
+		dashboardEl.appendChild(messageEl)
 
-			for (let i = 0; i<appointments.length; i++) {
-				let appointmentdate = new Date(appointments[i].Datum);
-				let day = Date.parse(new Date(appointmentdate.getFullYear(), appointmentdate.getMonth(), appointmentdate.getDate()));
-				let dateString = appointmentdate.toLocaleString('de-DE', {weekday: 'short', day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit'});
-				let appointmentEl = document.createElement('li');
-				let dateEl = document.createElement('span');
-				let courseEl = document.createElement('span');
-				let teacherEl = document.createElement('span');
-				let titleEl = document.createElement('a');
-				dateEl.innerText = `${dateString}:`;
-				//date.innerHTML += `&shy;`;
-				courseEl.innerText = `${appointments[i].Fach}`;
-				teacherEl.innerText = `${appointments[i].Lehrer}`;
-				titleEl.innerText = `${appointments[i].Titel}`;
-				titleEl.href=`termin/index.html?t=${appointments[i].TerminID}`; //////////////////////////////////////////////////// index.html
-				dateEl.classList.add('appointment-list-date');
-				courseEl.classList.add('appointment-list-course');
-				teacherEl.classList.add('appointment-list-teacher');
-				titleEl.classList.add('appointment-list-title');
-				appointmentEl.append(dateEl, courseEl, /*teacherEl,*/ titleEl);
+		let detailsElNow = document.createElement('details');
+		let summaryElNow = document.createElement('summary');
+		let listElNow = document.createElement('ul');
+		summaryElNow.innerText = "Termine der nächsten Woche";
+		detailsElNow.appendChild(summaryElNow);
+		detailsElNow.appendChild(listElNow);
+		detailsElNow.open = true;
+		let detailsElSoon = document.createElement('details');
+		let summaryElSoon = document.createElement('summary');
+		let listElSoon = document.createElement('ul');
+		summaryElSoon.innerText = "Termine der nächsten 30 Tage";
+		detailsElSoon.appendChild(summaryElSoon);
+		detailsElSoon.appendChild(listElSoon);
+		let detailsElLater = document.createElement('details');
+		let summaryElLater = document.createElement('summary');
+		let listElLater = document.createElement('ul');
+		summaryElLater.innerText = "Zukünftige Termine";
+		detailsElLater.appendChild(summaryElLater);
+		detailsElLater.appendChild(listElLater);
 
-				console.log(appointmentdate, day, todayDate)
-				if (day > todayDate-dayInMs*1 && day <= todayDate+dayInMs*7) {
-					listElNow.appendChild(appointmentEl);
-				} else if (day > todayDate-dayInMs*1 && day <= todayDate+dayInMs*30) {
-					listElSoon.appendChild(appointmentEl);
-				} else if (day > todayDate-dayInMs*1) {
-					listElLater.appendChild(appointmentEl);
-				}
-			}
+		let nowDate = new Date();
+		let todayDate = Date.parse(new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate()));
+		let dayInMs = 8.64e+7;
 
-			if (listElNow.innerHTML != ``) {
-				dashboardEl.appendChild(detailsElNow)
-			}
-			if (listElSoon.innerHTML != ``) {
-				dashboardEl.appendChild(detailsElSoon)
-			}
-			if (listElLater.innerHTML != ``) {
-				dashboardEl.appendChild(detailsElLater)
+		for (let i = 0; i<appointments.length; i++) {
+			let appointmentdate = new Date(appointments[i].Datum);
+			let day = Date.parse(new Date(appointmentdate.getFullYear(), appointmentdate.getMonth(), appointmentdate.getDate()));
+			let dateString = appointmentdate.toLocaleString('de-DE', {weekday: 'short', day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit'});
+			let appointmentEl = document.createElement('li');
+			let dateEl = document.createElement('span');
+			let courseEl = document.createElement('span');
+			let teacherEl = document.createElement('span');
+			let titleEl = document.createElement('a');
+			dateEl.innerText = `${dateString}:`;
+			//date.innerHTML += `&shy;`;
+			courseEl.innerText = `${appointments[i].Fach}`;
+			teacherEl.innerText = `${appointments[i].Lehrer}`;
+			titleEl.innerText = `${appointments[i].Titel}`;
+			titleEl.href=`termin/index.html?t=${appointments[i].TerminID}`; //////////////////////////////////////////////////// index.html
+			dateEl.classList.add('appointment-list-date');
+			courseEl.classList.add('appointment-list-course');
+			teacherEl.classList.add('appointment-list-teacher');
+			titleEl.classList.add('appointment-list-title');
+			appointmentEl.append(dateEl, courseEl, /*teacherEl,*/ titleEl);
+
+			console.log(appointmentdate, day, todayDate)
+			if (day > todayDate-dayInMs*1 && day <= todayDate+dayInMs*7) {
+				listElNow.appendChild(appointmentEl);
+			} else if (day > todayDate-dayInMs*1 && day <= todayDate+dayInMs*30) {
+				listElSoon.appendChild(appointmentEl);
+			} else if (day > todayDate-dayInMs*1) {
+				listElLater.appendChild(appointmentEl);
 			}
 		}
 
-	});
+		if (listElNow.innerHTML != ``) {
+			dashboardEl.appendChild(detailsElNow)
+		}
+		if (listElSoon.innerHTML != ``) {
+			dashboardEl.appendChild(detailsElSoon)
+		}
+		if (listElLater.innerHTML != ``) {
+			dashboardEl.appendChild(detailsElLater)
+		}
+	}
 
 }
 
--- frontend/termin/index.js
+++ frontend/termin/index.js
@@ -1,70 +1,72 @@
 const urlParams = new URLSearchParams(window.location.search);
 const searchID = urlParams.get('t');
 
+async function fetchAPI() {
+	let response = await fetch(APIURL+"/appointment/view", {
+		method: "POST",
+		body: JSON.stringify({
+			token: cookieToken,
+			terminid: searchID,
+		}),
+		headers: {
+			"Content-type": "application/json; charset=UTF-8"
+		}
+	});
+	let json = await response.json();
+	return await json.message;
+}
+
 const detailEl = document.getElementById('detail-view');
 async function updateDashboard() {
 
 	if (searchID) {
+		let appointments = await fetchAPI();
 
-		fetch(APIURL+"/appointment/view", {
-			method: "POST",
-			body: JSON.stringify({
-				token: cookieToken,
-				terminid: searchID,
-			}),
-			headers: {
-				"Content-type": "application/json; charset=UTF-8"
-			}
-		}).then((response) => response.json()).then((response) => {
-			let appointments = response.message
-
-			detailEl.innerHTML = ``;
-			if (appointments.length <= 0) { // change to if appointment does not exist
-				let messageEl = document.createElement('h3');
-				messageEl.innerText = 'Dieser Termin existiert nicht.';
-				detailEl.appendChild(messageEl)
-			} else {
-				let messageEl = document.createElement('h3');
-				messageEl.innerText = appointments.Titel;
-				detailEl.appendChild(messageEl)
-
-				let appointmentdate = new Date(appointments.Datum);
-				let day = Date.parse(new Date(appointmentdate.getFullYear(), appointmentdate.getMonth(), appointmentdate.getDate()));
-				let dateString = appointmentdate.toLocaleString('de-DE', {weekday: 'short', day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit'});
-				let appointmentEl = document.createElement('ul');
-				let dateEl = document.createElement('li');
-				let courseContainerEl = document.createElement('li');
-				let courseEl = document.createElement('span');
-				let teacherEl = document.createElement('li');
-				let teamEl = document.createElement('li');
-				let lastChangedByEl = document.createElement('li');
-				dateEl.innerText = `Datum: ${dateString}`;
-				courseContainerEl.innerText = `Fach: `;
-				courseEl.innerText = `${appointments.Fach}`;
-				teacherEl.innerText = `Lehrer: ${appointments.Lehrer}`;
-				teamEl.innerText = `Team: ${appointments.TeamName}`;
-				lastChangedByEl.innerText = `zuletzt geändert von: ${appointments.ZuletztGeaendertName}`;
-				dateEl.classList.add('appointment-detail-date');
-				courseEl.classList.add('appointment-detail-course');
-				teacherEl.classList.add('appointment-detail-teacher');
-				appointmentEl.classList.add('appointment-detail-view');
-				courseContainerEl.appendChild(courseEl);
-				appointmentEl.append(dateEl, courseContainerEl, teacherEl, teamEl, lastChangedByEl);
-
-				let notesEl = document.createElement('p');
-				notesEl.innerText = appointments.Notizen;
-
-				detailEl.append(appointmentEl, notesEl);
-			}
-
-		});
-
-	} else {
 		detailEl.innerHTML = ``;
-		let messageEl = document.createElement('h3');
-		messageEl.innerText = 'Fehler: Keine angegebene Termin-ID!'
-		detailEl.appendChild(messageEl)
-	}
+		if (appointments.length <= 0) { // change to if appointment does not exist
+			let messageEl = document.createElement('h3');
+			messageEl.innerText = 'Dieser Termin existiert nicht.';
+			detailEl.appendChild(messageEl)
+		} else {
+			let messageEl = document.createElement('h3');
+			messageEl.innerText = appointments.Titel;
+			detailEl.appendChild(messageEl)
+
+			let appointmentdate = new Date(appointments.Datum);
+			let day = Date.parse(new Date(appointmentdate.getFullYear(), appointmentdate.getMonth(), appointmentdate.getDate()));
+			let dateString = appointmentdate.toLocaleString('de-DE', {weekday: 'short', day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit'});
+			let appointmentEl = document.createElement('ul');
+			let dateEl = document.createElement('li');
+			let courseContainerEl = document.createElement('li');
+			let courseEl = document.createElement('span');
+			let teacherEl = document.createElement('li');
+			let teamEl = document.createElement('li');
+			let lastChangedByEl = document.createElement('li');
+			dateEl.innerText = `Datum: ${dateString}`;
+			courseContainerEl.innerText = `Fach: `;
+			courseEl.innerText = `${appointments.Fach}`;
+			teacherEl.innerText = `Lehrer: ${appointments.Lehrer}`;
+			teamEl.innerText = `Team: ${appointments.TeamName}`;
+			lastChangedByEl.innerText = `zuletzt geändert von: ${appointments.ZuletztGeaendertName}`;
+			dateEl.classList.add('appointment-detail-date');
+			courseEl.classList.add('appointment-detail-course');
+			teacherEl.classList.add('appointment-detail-teacher');
+			appointmentEl.classList.add('appointment-detail-view');
+			courseContainerEl.appendChild(courseEl);
+			appointmentEl.append(dateEl, courseContainerEl, teacherEl, teamEl, lastChangedByEl);
+
+			let notesEl = document.createElement('p');
+			notesEl.innerText = appointments.Notizen;
+
+			detailEl.append(appointmentEl, notesEl);
+		}
+
+} else {
+	detailEl.innerHTML = ``;
+	let messageEl = document.createElement('h3');
+	messageEl.innerText = 'Fehler: Keine angegebene Termin-ID!'
+	detailEl.appendChild(messageEl)
+}
 }
 
 updateDashboard()
