73183e4a4d5438d5f03df807445af57fcbf39dee
HackerCraft2000 <j.harnack@bbw-fi.de>
2026-01-26 07:57:03 +0100

    #53 und #54: Funktionen in history.js definiert und evtl. auskommentiert

--- backend/modules/history/history.js
+++ backend/modules/history/history.js
@@ -1,5 +1,13 @@
 import { query } from "../db/db.js";
 
 export async function listHistory(terminid) {
-    return query("SELECT ZuletztGeaendert, Datum, Titel, Fach, Lehrer FROM changes WHERE TerminID = ?", [terminid])
+    return query("SELECT AenderungsID, ZuletztGeaendert, Datum, Titel, Fach, Lehrer FROM changes WHERE TerminID = ?", [terminid])
+}
+
+export async function viewHistory(aenderungsid) {
+    return query("SELECT Timestamp, ZuletztGeaendert, TerminID, Datum, Titel, Fach, Lehrer, Notizen WHERE AenderungsID = ?", [aenderungsid])
+}
+
+export async function reverseChange(/*TBD*/) {
+    //Wird bei vorhandener Zeit umgesetzt
 }
